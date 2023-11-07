0005c559110abd23c4ba7a667d60394517f435d8
Fredrick Njuguna <kfred45.fk@gmail.com>
2023-11-07 19:48:20 +0300

    Playwright test

--- PlaywrightLoginTest/UnitTest1.cs
+++ PlaywrightLoginTest/UnitTest1.cs
@@ -1,15 +1,30 @@
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
 namespace PlaywrightLoginTest;
 
-public class Tests
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class Tests : PageTest
 {
     [SetUp]
-    public void Setup()
+    public async Task Setup()
     {
+        await Page.GotoAsync(url:"http://www.eaapp.somee.com");
     }
 
+
     [Test]
-    public void Test1()
+    public async Task Test1()
     {
-        Assert.Pass();
+        await Page.ClickAsync(selector:"text=Login");
+        await Page.FillAsync(selector:"#UserName", value:"admin");
+        await Page.FillAsync(selector:"#Password", value:"password");
+        await Page.ClickAsync(selector:"text=Log in");
+        await Expect(Page.Locator(selector:"text='Log off'")).ToBeVisibleAsync();
+        
     }
 }
